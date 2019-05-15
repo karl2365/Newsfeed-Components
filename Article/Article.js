906013b4b463e9336f0313932213f1b79881b599
Karl Beksa <karlbeksa@gmail.com>
2019-05-15 16:53:18 -0400

    stretch - close buttons better

--- Article/Article.js
+++ Article/Article.js
@@ -10,9 +10,7 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
     this.closeButton.textContent = "Close Article"
-    this.closeButton.style.display = "none";
-    this.closeButton.style.color = "red";
-
+   
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
     this.closeButton.addEventListener('click', () => this.closeArticle());
