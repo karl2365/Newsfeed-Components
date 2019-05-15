7a8fe1a86a67acdb2623fa3d4220bcf68d61f798
Karl Beksa <karlbeksa@gmail.com>
2019-05-15 16:49:12 -0400

    stretch - close buttons

--- Article/Article.js
+++ Article/Article.js
@@ -6,18 +6,28 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.domElement.querySelector('.expandButton');
+    this.closeButton = this.domElement.querySelector('.closeButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand";
+    this.closeButton.textContent = "Close Article"
+    this.closeButton.style.display = "none";
+    this.closeButton.style.color = "red";
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.closeButton.addEventListener('click', () => this.closeArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
-
+    this.closeButton.style.display = 'inline';
     this.expandButton.textContent === 'expand' ? this.expandButton.textContent = 'collapse' : this.expandButton.textContent = 'expand';
   }
+  closeArticle() {
+    this.domElement.style.display = 'none';
+  }
+
 }
 
 /* START HERE: 
